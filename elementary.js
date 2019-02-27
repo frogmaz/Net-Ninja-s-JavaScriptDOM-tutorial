9307698f5f03027f0a8ff3b4d17eeef80dacdb58
frogmaz <frogs.n.grandmas@gmail.com>
2019-02-27 10:42:23 +0100

    innerHTML, textContent and introduction to nodes

--- elementary.js
+++ elementary.js
@@ -1,11 +1,25 @@
 
+// selektovanje svog sadrzaja za postavljenom "adresom" u zagradi
+// da sam koristio querySelector (bez All) uzeo bi samo prvi element sa ove adrese
 var cards = document.querySelectorAll("#listContainer li .name");
 console.log(cards);
 
-var m = 1;
+//dodavanje tekstualnog HTML sadrzaja
 cards.forEach(function(card){
-  card.textContent = 'budala ' + m;
-  m++;
+  card.textContent += " (jest, jest)";
 })
 
-console.log(Array.from(cards));
+//dodavanje celog HTML elementa
+const cardList = document.querySelector('#listContainer ul');
+cardList.innerHTML += "<p> NEMA NITA </p>";
+
+//čvorovi unutar DOM-a
+const container = document.querySelector("#container");
+console.log(container.nodeType); //tip, odnosno nivo, čvora
+console.log(container.nodeName); //pod kojim imenom html elementa stoji ovaj čvor
+console.log(container.hasChildNodes()); //sadrži ili ne sadrži podelemente
+
+// kopiranje kontejnera
+// u zagradi TRUE da bi se kopirao CEO čvor sa svim svojim podelementima
+const clonedContainer = container.cloneNode(true);
+console.log(clonedContainer);
