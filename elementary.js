e6b297876a2bfc3f626560c8e417fde870b0ef43
frogmaz <frogs.n.grandmas@gmail.com>
2019-02-28 13:17:46 +0100

    transition with checkbox

--- elementary.js
+++ elementary.js
@@ -1,5 +1,6 @@
 const omotStavki =document.querySelector('#listContainer ul');
-//delete cards
+
+//delete notes
 omotStavki.addEventListener('click', function(e){
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
@@ -7,14 +8,13 @@ omotStavki.addEventListener('click', function(e){
   }
 })
 
-//add cards
+//add notes
 const dodajStavku = document.forms['addNote'];
 
 dodajStavku.addEventListener('submit',function(e){
   e.preventDefault();
   const upis = dodajStavku.querySelector('input[type="text"]').value;
   console.log(upis);
-
 
   //create elements
   const li =document.createElement('li');
@@ -27,11 +27,25 @@ dodajStavku.addEventListener('submit',function(e){
 
   //add classes
   liName.classList.add('name');
-  deleteBtn.classList.add('delete');
+  deleteBtn.classList.add('delete'); // .remove() za brisanje
 
   //append to document
   li.appendChild(liName);
   li.appendChild(deleteBtn);
   omotStavki.appendChild(li);
 
+  //removing typed text
+  dodajStavku.querySelector('input[type="text"]').value = '';
+
 })
+
+//hide notes
+const skrijListu = document.querySelector('#hide');
+
+skrijListu.addEventListener('change',function(e){
+  if(skrijListu.checked){
+    omotStavki.style.maxHeight = "0";
+  } else{
+    omotStavki.style.maxHeight = "260px";
+  }
+});
